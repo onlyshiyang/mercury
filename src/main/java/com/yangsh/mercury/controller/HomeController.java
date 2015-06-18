225bcfc3e60bea8bb00702b1b50bca5969962896
onlyshiyang <shiyang.personal@gmail.com>
2015-06-18 22:17:13 +0800

    minor update

--- src/main/java/com/yangsh/mercury/controller/HomeController.java
+++ src/main/java/com/yangsh/mercury/controller/HomeController.java
@@ -1,9 +1,17 @@
 package com.yangsh.mercury.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
 public class HomeController {
+
+  @RequestMapping(value = "/", method = GET)
+  public String home() {
+    
+    System.out.println("HomeController return view name.");
+    return "home";
+  }
 
 }
